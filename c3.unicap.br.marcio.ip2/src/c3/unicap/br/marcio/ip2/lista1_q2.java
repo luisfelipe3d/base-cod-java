f06e741bd44f65d27f7db122ce996e01db5150a7
luisfelipe3d <luisfelipe3d@protonmail.com>
2019-03-27 20:50:01 -0300

    fist commit

---
+++ c3.unicap.br.marcio.ip2/src/c3/unicap/br/marcio/ip2/lista1_q2.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package c3.unicap.br.marcio.ip2;
+
+/**
+ *
+ * @author user
+ */
+public class lista1_q2 {
+    public static void main(String[] args) {
+        System.out.println("Segundo teste");
+    }
+}
