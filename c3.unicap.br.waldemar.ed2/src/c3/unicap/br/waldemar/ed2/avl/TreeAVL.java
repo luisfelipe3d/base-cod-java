6d6eac2dd059e3260e775721dd428cf3db53b6e5
Luis Felipe <luisfelipe3d@protonmail.com>
2020-09-23 01:07:37 -0300

    avl tree

--- c3.unicap.br.waldemar.ed2/src/c3/unicap/br/waldemar/ed2/avl/TreeAVL.java
+++ c3.unicap.br.waldemar.ed2/src/c3/unicap/br/waldemar/ed2/avl/TreeAVL.java
@@ -5,10 +5,54 @@
  */
 package c3.unicap.br.waldemar.ed2.avl;
 
+import java.util.Set;
+
 /**
  *
  * @author home
  */
-public class TreeAVL {
+public class TreeAVL<KInteger, V> implements MyMap<KInteger, V>{
+    private TreeNodeAVL<KInteger, V> root;
+
+    @Override
+    public int size() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return this.root == null;
+    }
+
+    @Override
+    public boolean containsKey(Integer key) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public V get(Integer key) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void put(Integer key, V value) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public V remove(Integer key) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void clear() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Set<Integer> keySet() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
     
 }
---
+++ c3.unicap.br.waldemar.ed2/src/c3/unicap/br/waldemar/ed2/avl/TreeNodeAVL.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package c3.unicap.br.waldemar.ed2.avl;
+
+/**
+ *
+ * @author home
+ */
+class TreeNodeAVL<KInteger,V> {
+    private KInteger key;
+    private V value;
+    private int height;
+    private TreeNodeAVL left,right;
+    
+    TreeNodeAVL(KInteger key, V value){
+        this.key = key;
+        this.value = value;
+        this.height = 1;
+    }
+
+    public KInteger getKey() {
+        return key;
+    }
+
+    public void setKey(KInteger key) {
+        this.key = key;
+    }
+
+    public V getValue() {
+        return value;
+    }
+
+    public void setValue(V value) {
+        this.value = value;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public TreeNodeAVL getLeft() {
+        return left;
+    }
+
+    public void setLeft(TreeNodeAVL left) {
+        this.left = left;
+    }
+
+    public TreeNodeAVL getRight() {
+        return right;
+    }
+
+    public void setRight(TreeNodeAVL right) {
+        this.right = right;
+    }
+    
+    
+}
