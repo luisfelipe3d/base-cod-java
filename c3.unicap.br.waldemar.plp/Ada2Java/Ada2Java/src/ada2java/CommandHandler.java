d5db0401ab532d6b566dd852fcd3a941bdcd1cc1
joaothalis <55813342+joaothalis@users.noreply.github.com>
2020-10-10 22:48:11 -0300

    add commands

--- c3.unicap.br.waldemar.plp/Ada2Java/Ada2Java/src/ada2java/CommandHandler.java
+++ c3.unicap.br.waldemar.plp/Ada2Java/Ada2Java/src/ada2java/CommandHandler.java
@@ -5,15 +5,30 @@
  */
 package ada2java;
 
+import java.util.Scanner;
+
 /**
  *
  * @author joaot
  */
 public class CommandHandler implements AdaHandler{
 
+    //Put_Line("Digite seu nome: ");
+    //Put (N);
     @Override
     public void addLine(String s) {
+        String[] split = s.split("(");
+        split[1] = split[1].replaceAll(")", "");
+        split[1] = split[1].replaceAll(";", "");
         
+        if(split[0].startsWith("Put_Line")){
+            System.out.println(split[1]);
+        } else if(split[0].startsWith("Get")){
+            Scanner in = new Scanner(System.in);
+            //colocar o valor do scanner na variavel passada no get;
+        } else if(split[0].startsWith("Put")){
+            //
+        } //falta ler o if e for
     }
     
 }
