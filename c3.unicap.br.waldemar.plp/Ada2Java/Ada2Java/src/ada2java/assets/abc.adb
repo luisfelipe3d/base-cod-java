e80604cc7c414a071ecc701cbd01d1d517455b07
joaothalis <55813342+joaothalis@users.noreply.github.com>
2020-10-13 16:35:41 -0300

    Update abc.adb

--- c3.unicap.br.waldemar.plp/Ada2Java/Ada2Java/src/ada2java/assets/abc.adb
+++ c3.unicap.br.waldemar.plp/Ada2Java/Ada2Java/src/ada2java/assets/abc.adb
@@ -3,9 +3,7 @@ with Ada.Text_IO; use Ada.Text_IO;
 procedure abc is
    A : Integer := 7;
    B : Integer := 5;
-   C : Float := 2.0;
    D: Integer := B;
 begin
    Put_Line("Value of B: " & Integer'Image(B));
-   Put_Line("Value of C: " & Float'Image(C));
 end abc;
