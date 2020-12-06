affc65dc423bfaab3c53cce72539b364543161f2
Luis Felipe <luisfelipe3d@protonmail.com>
2020-12-06 19:39:42 -0300

    update

---
+++ c3.unicap.br.ana.ed1/classesPrincipais/src/ponto/extra/Main.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ponto.extra;
+
+/**
+ *
+ * @author home
+ */
+public class Main {
+    public static void main(String[] args) {
+        Tree<Integer> b = new Tree();
+        b.insert(10);
+        b.insert(30);
+    }
+}
